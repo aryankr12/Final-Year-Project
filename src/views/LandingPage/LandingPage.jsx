7b6584bf90f852862294c823dde70dce45d1854c
aryankr12 <kumaruttam813@gmail.com>
2021-04-07 11:20:48 +0530

    Updated React

---
+++ src/views/LandingPage/LandingPage.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+
+// @material-ui/icons
+
+// core components
+import Header from "components/Header/Header.jsx";
+import Footer from "components/Footer/Footer.jsx";
+import GridContainer from "components/Grid/GridContainer.jsx";
+import GridItem from "components/Grid/GridItem.jsx";
+import Button from "components/CustomButtons/Button.jsx";
+import HeaderLinks from "components/Header/HeaderLinks.jsx";
+import Parallax from "components/Parallax/Parallax.jsx";
+import LoginPage from "../LoginPage/LoginPage";
+
+import landingPageStyle from "assets/jss/material-kit-react/views/landingPage.jsx";
+
+// Sections for this page
+import ProductSection from "./Sections/ProductSection.jsx";
+import TeamSection from "./Sections/TeamSection.jsx";
+import WorkSection from "./Sections/WorkSection.jsx";
+import { Link } from "react-router-dom";
+
+const dashboardRoutes = [];
+
+class LandingPage extends React.Component {
+  render() {
+    const { classes, ...rest } = this.props;
+    return (
+      <div>
+        <Header
+          color="transparent"
+          routes={dashboardRoutes}
+          brand="Forecast"
+          rightLinks={<HeaderLinks />}
+          fixed
+          changeColorOnScroll={{
+            height: 400,
+            color: "white"
+          }}
+          {...rest}
+        />
+        <Parallax filter image={require("assets/img/landing-bg.jpg")}>
+          <div className={classes.container}>
+            <GridContainer>
+              <GridItem xs={12} sm={12} md={6}>
+                <h1 className={classes.title}>A Web App for predicting crop yield and prices.</h1>
+                <h4>
+                Designed & Developed By Vaibhav Prabhu and Team.
+                </h4>
+                <br />
+                <Link to={LoginPage}><Button
+                  color="danger"
+                  size="lg"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fas fa-rocket" />Join Now
+                </Button></Link>
+              </GridItem>
+            </GridContainer>
+          </div>
+        </Parallax>
+        <div className={classNames(classes.main, classes.mainRaised)}>
+          <div className={classes.container}>
+            <ProductSection />
+            <TeamSection />
+            <WorkSection />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default withStyles(landingPageStyle)(LandingPage);
---
+++ src/views/LandingPage/Sections/ProductSection.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+
+// @material-ui/icons
+import Chat from "@material-ui/icons/Chat";
+import VerifiedUser from "@material-ui/icons/VerifiedUser";
+import Fingerprint from "@material-ui/icons/Fingerprint";
+// core components
+import GridContainer from "components/Grid/GridContainer.jsx";
+import GridItem from "components/Grid/GridItem.jsx";
+import InfoArea from "components/InfoArea/InfoArea.jsx";
+
+import productStyle from "assets/jss/material-kit-react/views/landingPageSections/productStyle.jsx";
+
+class ProductSection extends React.Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.section}>
+        <GridContainer justify="center">
+          <GridItem xs={12} sm={12} md={8}>
+            <h2 className={classes.title}>Let's talk product</h2>
+            <h5 className={classes.description}>
+              We have Designed and Developed a Website that could Be used for predicting future
+              crop prices of different states and what would be the their price.
+            </h5>
+          </GridItem>
+        </GridContainer>
+        <div>
+          <GridContainer>
+            <GridItem xs={12} sm={12} md={4}>
+              <InfoArea
+                title="Free Chat"
+                description="Divide details about your product or agency work into parts. Write a few lines about each one. A paragraph describing a feature will be enough."
+                icon={Chat}
+                iconColor="info"
+                vertical
+              />
+            </GridItem>
+
+          </GridContainer>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withStyles(productStyle)(ProductSection);
